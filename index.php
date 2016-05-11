2cf1944568d93e6f587b48160029b708545aeab0
Robert Bunch <rob@digitalcrafts.com>
2016-05-11 10:00:33 -0400

    Header roughly complete

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title></title>
 
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 	<script type="text/javascript" src="js/scripts.js"></script>
@@ -12,17 +13,23 @@
 <body>
 
 	<div id="header">
-		<div id="header-left">
-			<div id="logo">
-				<img src="images/default-sf-logo.png">
-			</div>
-		</div>	
-		<div id="header=right">
-			<div id="top-links">
-				<li>Park</li>
-				<li>HomeNational</li>
-				<li>HomePark</li>
-				<li>MapPass Holder Login</li>
+		<div id="header-inner">
+			<div id="header-left">
+				<div id="logo">
+					<img src="images/default-sf-logo.png">
+				</div>
+			</div>	
+			<div id="header-right">
+				<div id="top-links">
+					<li class="link-item"><a href="#">Park</a></li>
+					<li class="link-item"><a href="#">HomeNational</a></li>
+					<li class="link-item"><a href="#">HomePark</a></li>
+					<li class="link-item"><a href="#">MapPass Holder Login</a></li>
+					<li class="link-item orange"><a href="#"><img src="images/icon-cart.png"> My Cart [0]</a></li>
+					<li class="link-item orange"><a href="#">Enter Promo Code</a></li>
+				</div>
+				<div id="park-title">Six Flags White Water</div>
+				<div id="park-city">Atlanta</div>
 			</div>
 		</div>
 	</div>
