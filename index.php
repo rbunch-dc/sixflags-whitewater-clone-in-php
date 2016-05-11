e2ceaca65a6dc03defde4776e586ea0e58515498
Robert Bunch <rob@digitalcrafts.com>
2016-05-11 09:27:58 -0400

    repo init

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+
+</head>
+<body>
+
+	<div id="header">
+		<div id="header-left">
+			<div id="logo">
+				<img src="images/default-sf-logo.png">
+			</div>
+		</div>	
+		<div id="header=right">
+			<div id="top-links">
+				<li>Park</li>
+				<li>HomeNational</li>
+				<li>HomePark</li>
+				<li>MapPass Holder Login</li>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
